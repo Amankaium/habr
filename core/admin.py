205d961ca55ade55f7d926b1ce1df1921dc3ecb2
Amankaium <kaium@list.ru>
2021-04-07 15:05:44 +0600

    add article admin

--- core/admin.py
+++ core/admin.py
@@ -2,6 +2,21 @@ from django.contrib import admin
 from core.models import Article, Author, ArticleImage
 
 # Register your models here.
-admin.site.register(Article)
+
 admin.site.register(Author)
 admin.site.register(ArticleImage)
+
+class ArticleAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Article
+
+    list_display = ('title', 'author', 'views', 'is_active', 'updated_at')
+    list_editable = ('author', 'is_active')
+    ordering = ['title']
+    list_filter = ['is_active', 'updated_at']
+    search_fields = ['title', 'text']
+
+    fields = ('title', 'text', 'views', 'created_at', 'updated_at', 'readers')
+    readonly_fields = ('views', 'created_at', 'updated_at', 'readers')
+
+admin.site.register(Article, ArticleAdmin)
